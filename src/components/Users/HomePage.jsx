d06c2ab4fd69271666887d3d9b377de8d9ad94f0
SahilM2063 <makadiasahil9846@gmail.com>
2024-03-02 19:08:38 +0530

    Brand Logo slider Implemented through react-fast-marquee library

--- src/components/Users/HomePage.jsx
+++ src/components/Users/HomePage.jsx
@@ -5,6 +5,7 @@ import h2 from "../../assets/h2.png";
 import h3 from "../../assets/h3.png";
 import h4 from "../../assets/h4.png";
 import main from "../../assets/main.svg";
+import Marquee from "react-fast-marquee";
 import brand1 from "../../assets/brand1.svg";
 import brand2 from "../../assets/brand2.svg";
 import brand3 from "../../assets/brand3.svg";
@@ -14,28 +15,36 @@ import brand5 from "../../assets/brand5.svg";
 const HomePage = () => {
   return (
     <>
-      <div className="w-full h-[calc(100vh-100px)] px-32 py-4 flex justify-center gap-8 select-none">
-        <img src={h1} alt="No img" className="w-[24%]" />
-
-        <div className="w-[26%] flex flex-col justify-between h-full">
-          <img src={h3} alt="No img" />
-          <div className="w-full flex flex-col items-center gap-6 mt">
-            <img src={main} alt="No img" className="w-full" />
-            <button className="py-2 px-8 bg-black text-white rounded-lg">
-              Shop Now
+      <div className="w-full sm:h-[calc(100vh-100px)] md:h-[calc(100vh-100px)] grid grid-cols-3 sm:grid-cols-1 sm:place-content-center px-32 md:px-10 py-4 gap-8 select-none">
+        <div className="img__Container h-full rounded-lg overflow-hidden sm:hidden">
+          <img src={h1} alt="No image" className="w-full h-full object-cover" />
+        </div>
+        <div className="main__Container h-full flex flex-col justify-between gap-2">
+          <div className="sub_img_Container sm:hidden">
+            <img src={h3} alt="No image" />
+          </div>
+          <div className="sub_main_Container flex flex-col items-center gap-4">
+            <img src={main} alt="No image" />
+            <button className="py-3 px-12 bg-black text-white rounded-lg uppercase text-sm tracking-wide">
+              Shop now
             </button>
           </div>
-          <img src={h4} alt="No img" />
+          <div className="sub_img_Container sm:hidden">
+            <img src={h4} alt="No image" />
+          </div>
         </div>
-
-        <img src={h2} alt="No img" className="w-[24%]" />
+        <div className="img__Container h-full rounded-lg overflow-hidden sm:hidden">
+          <img src={h2} alt="No image" className="w-full h-full object-cover" />
+        </div>
       </div>
-      <div className="brand-stripe w-full h-[100px] flex items-center gap-20 justify-center">
-        <img src={brand1} alt="brand1" className="w-[8%]" />
-        <img src={brand2} alt="brand2" className="w-[8%]" />
-        <img src={brand3} alt="brand3" className="w-[8%]" />
-        <img src={brand4} alt="brand4" className="w-[8%]" />
-        <img src={brand5} alt="brand5" className="w-[8%]" />
+      <div className="brand-stripe w-full h-[80px] flex items-center gap-20 justify-center">
+        <Marquee play pauseOnHover autoFill gradient speed={200}>
+          <img src={brand1} alt="brand1" className="mx-10 w-[40%]" />
+          <img src={brand2} alt="brand2" className="mx-10 w-[40%]" />
+          <img src={brand3} alt="brand3" className="mx-10 w-[40%]" />
+          <img src={brand4} alt="brand4" className="mx-10 w-[40%]" />
+          <img src={brand5} alt="brand5" className="mx-10 w-[40%]" />
+        </Marquee>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': { 'max': '426px' },
+      'md': { 'max': '768px' },
+      'lg': { 'min': '769px' }
+    },
+    extend: {
+      keyframes: {
+        slide: {
+          'from': { transform: 'translateX(0)' },
+          'to': { transform: 'translateX(-100%)' },
+        }
+      }
+
+    },
     plugins: [require('daisyui')],
   }
 
