cdce66d448e511353f1a2405a5f43b2909d8cb08
SahilM2063 <makadiasahil9846@gmail.com>
2024-04-10 20:49:36 +0530

    Handling Backend Changes And Some Improvements

--- src/components/Admin/Home.jsx
+++ src/components/Admin/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { Children, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllOrders, getOrderStats } from "../../redux/slices/orderSlice";
 import { getAllUsersAction } from "../../redux/slices/userSlice";
@@ -13,10 +13,7 @@ const Home = () => {
     dispatch(getOrderStats());
   }, [dispatch]);
 
-  const {
-    orderStats,
-    orders: { orders },
-  } = useSelector((state) => state?.orders);
+  const { orderStats } = useSelector((state) => state?.orders);
   const mainData = orderStats?.orderStats?.[0];
   // console.log(mainData);
 
@@ -24,7 +21,7 @@ const Home = () => {
 
   return (
     <div>
-      <div className="statsLine w-full grid grid-cols-4 gap-6">
+      <div className="statsLine w-full grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6">
         <div className="p-6 bg-[#E3F5FF] rounded-2xl flex flex-col justify-between items-start gap-3">
           <h1 className="font-[Poppins] font-semibold">Total Sales</h1>
           <span className="font-[Poppins] text-3xl font-bold">
@@ -34,7 +31,7 @@ const Home = () => {
         <div className="p-6 bg-[#E5ECF6] rounded-2xl flex flex-col justify-between items-start gap-3">
           <h1 className="font-[Poppins] font-semibold">Total Orders</h1>
           <span className="font-[Poppins] text-3xl font-bold">
-            {orders?.length}
+            {mainData?.totalOrders}
           </span>
         </div>
         <div className="p-6 bg-[#E3F5FF] rounded-2xl flex flex-col justify-between items-start gap-3">
