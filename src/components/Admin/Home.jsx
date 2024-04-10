50d0d02dfa218d6394e2c75f3c35a91def7375b3
SahilM2063 <makadiasahil9846@gmail.com>
2024-04-10 16:25:04 +0530

    Fetching And Showing Necessary data

--- src/components/Admin/Home.jsx
+++ src/components/Admin/Home.jsx
@@ -1,33 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from "react";
+import React, { Children, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getOrderStats } from "../../redux/slices/orderSlice";
+import { getAllOrders, getOrderStats } from "../../redux/slices/orderSlice";
+import { getAllUsersAction } from "../../redux/slices/userSlice";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(getAllUsersAction());
+    dispatch(getAllOrders());
     dispatch(getOrderStats());
   }, [dispatch]);
+
+  const {
+    orderStats,
+    orders: { orders },
+  } = useSelector((state) => state?.orders);
+  const mainData = orderStats?.orderStats?.[0];
+  // console.log(mainData);
+
+  const { users } = useSelector((state) => state?.users?.users);
 
   return (
     <div>
       <div className="statsLine w-full grid grid-cols-4 gap-6">
         <div className="p-6 bg-[#E3F5FF] rounded-2xl flex flex-col justify-between items-start gap-3">
           <h1 className="font-[Poppins] font-semibold">Total Sales</h1>
-          <span className="font-[Poppins] text-3xl font-bold">₹ 100K</span>
+          <span className="font-[Poppins] text-3xl font-bold">
+            ₹ {mainData?.sumOfSales}
+          </span>
         </div>
         <div className="p-6 bg-[#E5ECF6] rounded-2xl flex flex-col justify-between items-start gap-3">
           <h1 className="font-[Poppins] font-semibold">Total Orders</h1>
-          <span className="font-[Poppins] text-3xl font-bold">456</span>
+          <span className="font-[Poppins] text-3xl font-bold">
+            {orders?.length}
+          </span>
         </div>
         <div className="p-6 bg-[#E3F5FF] rounded-2xl flex flex-col justify-between items-start gap-3">
           <h1 className="font-[Poppins] font-semibold">Avg. Order</h1>
-          <span className="font-[Poppins] text-3xl font-bold">₹ 879.89</span>
+          <span className="font-[Poppins] text-3xl font-bold">
+            ₹ {mainData?.averageOrder?.toFixed(2)}
+          </span>
         </div>
         <div className="p-6 bg-[#E5ECF6] rounded-2xl flex flex-col justify-between items-start gap-3">
           <h1 className="font-[Poppins] font-semibold">Active Users</h1>
-          <span className="font-[Poppins] text-3xl font-bold">340</span>
+          <span className="font-[Poppins] text-3xl font-bold">
+            {users?.length}
+          </span>
         </div>
       </div>
     </div>
