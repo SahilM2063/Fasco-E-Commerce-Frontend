f4592801bb0c114dc5ab953f492d7aa741373aba
Sahil Makadia <makadiasahil9846@gmail.com>
2025-06-30 23:37:59 +0530

    changed upd

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
-// const baseURL = "https://fasco-ecommerce-backend.onrender.com/api/v1";
-const baseURL = "http://localhost:6969/api/v1";
+const baseURL = "https://e-commerce-backend-5l0o.onrender.com/api/v1";
+// const baseURL = "http://localhost:6969/api/v1";
 
 
 export default baseURL;
